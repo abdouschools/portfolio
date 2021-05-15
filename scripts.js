5ac58c4951929514c2dc7d4b9863257dd9e8a489
abdouschools <abdouschools@gmail.com>
2021-05-15 19:49:19 +0200

    responsive section 1

--- scripts.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-    $(window).scroll(function(){
-        if(this.scrollY > 40 ){
-            $('.navbar').addClass("sticky")
-        }else{ $('.navbar').removeClass("sticky")}
-    })
-})
