2b8c591a3109f53ec66861f2d3ece9fb1f142e48
abdouschools <abdouschools@gmail.com>
2021-05-15 14:23:51 +0200

    creation section 1

---
+++ scripts.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if(this.scrollY > 40 ){
+            $('.navbar').addClass("sticky")
+        }else{ $('.navbar').removeClass("sticky")}
+    })
+})
