6fde021617aaff52d47ae0a91b0a0319a1535055
abdouschools <abdouschools@gmail.com>
2021-05-29 20:08:17 +0200

    modification de contenue

--- index.php
+++ index.php
@@ -21,12 +21,12 @@
         <div class="max-width">
             <div class="logo"><a href="">Portfo<span>lio</span></a></div>
             <ul class="menu">
-                <li><a href="#home">Home</a></li>
-                <li><a href="#about">About</a></li>
+                <li><a href="#home">Accueil</a></li>
+                <li><a href="#about">A-propos</a></li>
                 <li><a href="#services">Services</a></li>
-                <li><a href="#skills">Skills</a></li>
+                <li><a href="#skills">Compétences</a></li>
                 <li><a href="">Teams</a></li>
-                <li><a href="#contact">Contact</a></li>
+                <li><a href="#contact">Contactez moi</a></li>
             </ul>
             <div class="menu-btn">
                 <i class="fas fa-bars"></i>
@@ -37,23 +37,32 @@
     <section class="home" id="home">
         <div class="max-width">
             <div class="home-content">
-                <div class="text-1">hello my name is </div>
+                <div class="text-1">Salut mon nom et </div>
                 <div class="text-2">Benyahia Abdennour</div>
-                <div class="text-3">And I'm a <span class='typing'></span></div>
-                <a href="#contact">Hire me</a>
+                <div class="text-3">Et je suis un <span class='typing'></span></div>
+                <a href="#contact">Engagez moi</a>
             </div>
         </div>
     </section>
     <section class="about" id="about">
         <div class="max-width">
-            <h2 class="title">About Me</h2>
+            <h2 class="title">A-propos </h2>
             <div class="about-content">
                 <div class="column left">
                     <img src="images/photo.jpg" alt="">
                 </div>
                 <div class="column right">
-                    <div class="text">I'm Benyahuia and I'm A <span class='typing-2'></span></div>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat deserunt laborum dolores consectetur voluptates, saepe iure aut dicta, dolorum voluptatum praesentium! Ex non rerum alias sunt. Adipisci quas laborum doloribus aperiam doloremque eaque nisi sequi enim sit hic animi distinctio, repellat accusamus non molestiae obcaecati quidem esse ipsa nobis. Illum rem consectetur cum, voluptatem perferendis veniam a molestiae voluptatum sequi.</p>
+                    <div class="text">Je suis Benyahia un <span class='typing-2'></span></div>
+                    <p>Bonjour, je suis entré dans le domaine du développement web en 2019 en m'appuyant sur moi pour apprendre et j'ai poursuivi ce parcours pédagogique en suivant une formation professionnelle à Doranco Ecole Supérieure des Technologies Créatives où j'ai obtenu mon diplôme avec distinction (Titre RNCP Bac+2 DÉVELOPPEUR WEB ET WEBMOBILE)
+                        Je suis quelqu'un de curieux et passionné, j'aime expérimenter, découvrir et apprendre au fur et à mesure de mes projets professionnels et personnels.
+                        Aujourd'hui, je met à profit mes connaissances pour les missions suivantes en freelance:
+
+                        -Intégration HTML / CSS à partir de vos maquettes graphiques
+                        - Site responsive «Adaptable sur tous types d'écrans»
+                        -Création des sites et thèmes avec le CMS Wordpres
+                        -Création des sites avec Symfony
+                        -Création des sites vitrine /Blog / e-commerce
+                        En parallèle, je continue à moi sur les nouvelles technologies afin d’augmenter mes compétences.</p>
                     <a href="cv.pdf" class="button" download><i class="fa fa-download"></i>Download CV</a>
                 </div>
             </div>
@@ -61,27 +70,29 @@
     </section>
     <section class="services" id="services">
         <div class="max-width">
-            <h2 class="title">My Services</h2>
+            <h2 class="title">Mes services</h2>
             <div class="serv-content">
                 <div class="card">
                     <div class="box">
                         <i class="fas fa-paint-brush"></i>
-                        <div class="text">Web Design</div>
-                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repudiandae vel voluptatum sequi itaque voluptas repellendus.</p>
+                        <div class="text">Application front-end back-end</div>
+                        <p>Développement d'application web front-end avec html-css-bootstarp-sass-scss ,back-end symfony-javascript-php-jquerry ,site vitrine ,site e-commerce ,intégration du front-end à votre API.</p>
                     </div>
                 </div>
                 <div class="card">
                     <div class="box">
                         <i class="fas fa-chart-line"></i>
                         <div class="text">Web Design</div>
-                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repudiandae vel voluptatum sequi itaque voluptas repellendus.</p>
+                        <p>Logos, templates Web, plaquettes publicitaires,
+                            cartes de visite, newsletters...</p>
                     </div>
                 </div>
                 <div class="card">
                     <div class="box">
                         <i class="fas fa-code"></i>
-                        <div class="text">Apps Design</div>
-                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repudiandae vel voluptatum sequi itaque voluptas repellendus.</p>
+                        <div class="text">Référencement naturel</div>
+                        <p>Affichage sémantique des informations,
+                            des pages propres pour un référencement optimal.</p>
                     </div>
                 </div>
             </div>
@@ -89,11 +100,19 @@
     </section>
     <section class="skills" id="skills">
         <div class="max-width">
-            <h2 class="title">My Skills</h2>
+            <h2 class="title">Compétences</h2>
             <div class="skills-content">
                 <div class="column left">
-                    <div class="text">My Skills & Expriences</div>
-                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Itaque, amet rem. Iusto optio expedita est in impedit, facilis mollitia minima perferendis cum dolore sequi vero officiis eius eligendi vitae nemo placeat neque maiores modi ducimus suscipit. Eos aut culpa ipsam quibusdam a, non id ipsum eius numquam fugit voluptatem laborum error velit voluptates deserunt ab autem sint inventore provident consequatur.</p>
+                    <div class="text">Mes compétences et expériences</div>
+                    <p>-Intégration (X)HTML / CSS.</p>
+                    <p>Codage à la main respectueux des standards du Web</p>
+                    <p>-Conception milti-plateformes.
+                    <p>Compatible tous supports, tablette & application mobile</p>
+                    <p>-validation W3C, réferencement naturel SEO.</p>
+                    <p>Accessibilité & ergonomie des pages web</p>
+                    <p>-Applications spécifiques et interface d'administartion PHP - MYSQL.</p>
+                    <p>-Gestion de projet web
+                        Site vitrine, corporate, e-commerce, application mobile.</p>
                     <a href="https://github.com/abdouschools">See my projects</a>
                 </div>
                 <div class="column right">
@@ -138,23 +157,23 @@
     </section>
     <section class="contact" id="contact">
         <div class="max-width">
-            <h2 class="title">Contact Me</h2>
+            <h2 class="title">Contact</h2>
             <div class="contact-content">
                 <div class="column left">
-                    <div class="text">Get In touch</div>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque numquam obcaecati suscipit accusamus quibusdam sint nobis ratione </p>
+                    <div class="text">Entrer en contact</div>
+                    <p>Une Idée ? Un Projet ? N'hésitez pas a Demandez un devis ! [GRATUIT]</p>
                     <div class="icons">
                         <div class="row">
                             <i class="fas fa-user"></i>
                             <div class="info">
-                                <div class="head">name</div>
+                                <div class="head">Nom</div>
                                 <div class="sub-title">Benyahia Abdennour</div>
                             </div>
                         </div>
                         <div class="row">
                             <i class="fas fa-map-marker-alt"></i>
                             <div class="info">
-                                <div class="head">Address</div>
+                                <div class="head">Adresse</div>
                                 <div class="sub-title">Chelles 77500</div>
                             </div>
                         </div>
@@ -168,7 +187,7 @@
                     </div>
                 </div>
                 <div class="column right">
-                    <div class="text">Message Me</div>
+                    <div class="text">Envoie moi un message</div>
                     <form action="mailer.php" method="POST">
                         <div class="fields">
                             <div class="field name">
@@ -185,7 +204,7 @@
                             <textarea name="message" cols="30" rows="10" placeholder="Describtion..."></textarea>
                         </div>
                         <div class="button">
-                            <button type="submit">Send Message</button>
+                            <button type="submit">Envoyer</button>
                         </div>
 
                     </form>
@@ -195,7 +214,7 @@
         </div>
     </section>
     <footer>
-        <span>Created By <a href="https://github.com/abdouschools">AbdouSchools</a> | <span class="far fa-copyright"></span>2020ALL rigts reserved.</span>
+        <span>Créé par <a href="https://github.com/abdouschools">AbdouSchools</a> | <span class="far fa-copyright"></span>2020 Tous les droits sont réservés.</span>
     </footer>
     <script src="script.js">
     </script>
